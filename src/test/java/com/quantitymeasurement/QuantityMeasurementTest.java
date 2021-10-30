79edfc2d96449137ceec5a2c60fa967b040efccb
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-10-30 20:51:43 +0530

    Added functionality: code and test case to check equality of 0 feet and 1 feet

--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -10,6 +10,13 @@ public class QuantityMeasurementTest {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
-
     }
+
+    @Test
+    public void Given0FeetAnd1Feet_ShouldNotReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
 }
