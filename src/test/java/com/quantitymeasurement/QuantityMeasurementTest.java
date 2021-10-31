db83677ba1be64f573890ccf55fcbbe3d4f0ed57
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-10-31 14:35:48 +0530

    Added feature:Code and testcase for equality of 0 inch and 0 inch

--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -40,4 +40,10 @@ public class QuantityMeasurementTest {
         Assert.assertNotEquals( feet1, feet2 );
     }
 
+    @Test
+    public void Given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals( inch1, inch2 );
+    }
 }
